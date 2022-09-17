25401893073afd6e72ec933acb18812ad254e3ac
unknown <cai.gil.tomas001@gmail.com>
2022-09-17 16:15:09 -0300

    3er ejercicio de la primer clase

---
+++ EJERCICIOS/Ejercicio 3 de la primer clase de python.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     17/09/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #Le pido al usuario que ingrese su nombre y DNI mediante el "input" y luego con la función "f" las ingreso dentro del texto
+    Nombre = input("Ingrese su nombre: ")
+    Dni = input("Ingrese su DNI: ")
+    print(f"El nombre del usuario es {Nombre} y su DNI es {Dni}")
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()

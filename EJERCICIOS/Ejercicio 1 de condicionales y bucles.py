a58a0ba353d213646b6e883cad5a59fc6f212b91
unknown <cai.gil.tomas001@gmail.com>
2022-09-17 19:27:34 -0300

    Primer ejercicios de condicionales y bucles

---
+++ EJERCICIOS/Ejercicio 1 de condicionales y bucles.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     17/09/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #Ejercicio 1 de condicionales y bucles - ingresar un numero impar
+    numeroimpar = (int(input("Ingrese un numero impar: ")))
+    while numeroimpar % 2 == 0:
+        numeroimpar = (int(input("Error, ingrese un numero impar: ")))
+    print(f"El numero ingresado es impar: {numeroimpar}")
+
+
+
+
+if __name__ == '__main__':
+    main()

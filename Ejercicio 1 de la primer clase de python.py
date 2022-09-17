6d7124da798628cd4c3d4c6de52a4b6eb8c0dd75
unknown <cai.gil.tomas001@gmail.com>
2022-09-17 15:30:07 -0300

    Ejercicio uno de la primer clase de python

---
+++ Ejercicio 1 de la primer clase de python.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     17/09/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #Ejercicio 1, cree dos variables de distinto valor numerico para luego ejecurtarlas restandolas
+    numerouno = 17
+    numerodos = 10
+    print(numerouno - numerodos)
+
+
+
+
+
+if __name__ == '__main__':
+    main()

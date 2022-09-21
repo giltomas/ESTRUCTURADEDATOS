84e52db877f8ddcb6ae9019d8ef035744543a266
unknown <cai.gil.tomas001@gmail.com>
2022-09-20 22:29:11 -0300

    subo el ejercicio numero 4 de condicionales y bucles, no se si esta bien

---
+++ EJERCICIOS/CONDICIONALES-Y-BUCLES-EJERCICIO-4.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     20/09/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #Ejercicio numero 4 de condicionales y bucles
+    milista = ["Boca", "River", "Independiente", "Racing", "San Lorenzo"]
+    contador = 0
+    for caracter in milista:
+        print(caracter)
+        contador = contador + 1
+    print("La cantidad de elementos de la lista es", contador)
+
+
+
+
+if __name__ == '__main__':
+    main()

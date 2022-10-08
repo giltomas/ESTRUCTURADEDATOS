5493ed3c5c1cbd6e010b47745420e9fb424e4be3
unknown <cai.gil.tomas001@gmail.com>
2022-10-08 20:54:59 -0300

    ejercicio 5 de condicionales y bucles

---
+++ EJERCICIOS/CONDICIONALES-Y-BUCLES-EJERCICIO-5.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     08/10/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #ejercicio numero 5, condicionales y bucles
+    lista = [2, 4, 6, 8, 10]
+    suma = 0
+    for i in lista:
+        suma += i
+        print(suma)
+
+
+
+
+if __name__ == '__main__':
+    main()

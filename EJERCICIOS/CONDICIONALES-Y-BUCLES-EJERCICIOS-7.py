a25cfbc74d7b3a70e244387dfd7b395a2b188e07
unknown <cai.gil.tomas001@gmail.com>
2022-10-08 21:38:20 -0300

    Ejercicio numero 7 de condicionales y bucles

---
+++ EJERCICIOS/CONDICIONALES-Y-BUCLES-EJERCICIOS-7.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     08/10/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #Ejercicio numero 7 de condicionales y bucles, no utilizar max
+    lista = [2, 6, 12, 24, 48, 96]
+    mayor = 0
+    for i in lista:
+        if i>mayor:
+            mayor = i
+    print(mayor)
+
+
+
+
+if __name__ == '__main__':
+    main()

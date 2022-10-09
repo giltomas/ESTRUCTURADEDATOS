7c32db81af68dd3c2da2d471bba858ea5b4ee106
unknown <cai.gil.tomas001@gmail.com>
2022-10-09 20:01:02 -0300

    modifique el ejercicio por un error

--- EJERCICIOS/CONDICIONALES-Y-BUCLES-EJERCICIO-2.py
+++ EJERCICIOS/CONDICIONALES-Y-BUCLES-EJERCICIO-2.py
@@ -10,11 +10,20 @@
 #-------------------------------------------------------------------------------
 
 def main():
-    contador = 10
-    while contador>0:
-        print("entende cintia")
-        contador -= 1
-    print("no era tan dificil viste")
+    numero1 = int(input("Ingrese su primer numero: "))
+    numero2 = int(input("Ingrese su segundo numero: "))
+    print("Elija una de las 3 opciones")
+    opcion = int(input("1. Sumar, 2. Restar, 3. Multiplicar: "))
+    while opcion<1 or opcion>3:
+        print("opción incorrecta, vuelva a ingresar una opción")
+        opcion = int(input("1. Sumar, 2. Restar, 3. Multiplicar: "))
+    if opcion==1:
+        print("La opción elegida es suma", numero1, "+", numero2, "=", numero1 + numero2)
+    if opcion==2:
+        print("La opción elegida es resta", numero1, "-", numero2, "=", numero1 - numero2)
+    if opcion==3:
+        print("La opción elegida es multiplicación", numero1, "x", numero2, "=", numero1 * numero2)
+
 
 
 

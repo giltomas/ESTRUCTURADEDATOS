35072e0ddffb0df73d023ccbd253fb25f6f77fc5
unknown <cai.gil.tomas001@gmail.com>
2022-10-10 12:18:27 -0300

    Ejercicio 1 de funciones

---
+++ EJERCICIOS/FUNCIONES-EJERCICIO-1.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     10/10/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    def area_rectangulo(base, altura):
+        return base * altura
+
+    area = area_rectangulo(5, 10)
+    print(area)
+
+if __name__ == '__main__':
+    main()

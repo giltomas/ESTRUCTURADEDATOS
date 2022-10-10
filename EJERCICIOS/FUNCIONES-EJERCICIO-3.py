89438ea6e2d5c37b2fdba74c226c15fcba235302
unknown <cai.gil.tomas001@gmail.com>
2022-10-10 12:29:15 -0300

    Subo ejercicio 3 de funciones

---
+++ EJERCICIOS/FUNCIONES-EJERCICIO-3.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     10/10/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    def relacion(nro1, nro2):
+        if nro1>nro2:
+            print (1)
+        elif nro1<nro2:
+            print (-1)
+        else:
+            print (0)
+
+    relacion(1, 1)
+
+if __name__ == '__main__':
+    main()

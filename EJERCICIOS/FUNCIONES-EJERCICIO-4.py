f918e9567bc561747cc859729ec6dff3bb5fdbb1
unknown <cai.gil.tomas001@gmail.com>
2022-10-10 12:34:28 -0300

    Subo el ejercicio 4 de funciones

---
+++ EJERCICIOS/FUNCIONES-EJERCICIO-4.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     10/10/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    def intermedio(nro1, nro2):
+        return (nro1 + nro2) // 2
+
+    medio = intermedio(10, 12)
+    print(medio)
+
+
+if __name__ == '__main__':
+    main()

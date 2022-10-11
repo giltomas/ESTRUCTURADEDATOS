edcce072d2cbc0139a44052cf9ec13275a79e68d
unknown <cai.gil.tomas001@gmail.com>
2022-10-11 13:20:03 -0300

    Subo el ejercicio 4 de funciones recursivas

---
+++ EJERCICIOS/FUNCIONES-RECURSIVAS/EJERCICIO-4.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      Tomas
+#
+# Created:     11/10/2022
+# Copyright:   (c) Tomas 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    #Escribir una función recursiva que permita mostrar los valores de un vector de atrás hacia adelante.
+    def mostrar(lista):
+        lista.reverse()
+        for i in lista:
+            if lista != []:
+                print(i)
+            else:
+                return
+
+    lista1 = [1, 2, 3, 4, 5]
+    mostrar(lista1)
+
+if __name__ == '__main__':
+    main()
